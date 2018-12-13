326a7c2851beed1aac54487a4582cd2026bb5d4e
JongSoo Park <newtein80@gmail.com>
2018-12-14 00:20:30 +0900

    2018.12.14 일단 Detail 까지.

--- WebCore/Areas/VulnManage/Controllers/VulnManageController.cs
+++ WebCore/Areas/VulnManage/Controllers/VulnManageController.cs
@@ -28,6 +28,47 @@ namespace WebCore.Areas.VulnManage.Controllers
         }
 
         [HttpGet]
+        public IActionResult VulnCreate()
+        {
+            //ViewData["GroupSeq"] = new SelectList(_context.TvulnGroup, "GroupSeq", "GroupType");
+            return View();
+        }
+
+        // GET: VulnManage/Tvulns/Details/5
+        [HttpGet]
+        public IActionResult VulnDetail(long? id)
+        {
+            if (id == null || 1 > id)
+            {
+                return NotFound();
+            }
+
+            var param = new DynamicParameters();
+            param.Add("@vuln_seq", id);
+            param.Add("@comp_seq", 0);
+            param.Add("@diag_kind", "");
+            param.Add("@group_seq", 0);
+
+            // 테이블 한개 리스트
+            T_VULN t_VULN = _dapperHelper.Top1<T_VULN>("SP_VULN_INFO_VIW", param);
+
+            //string m_UseYn = "Y";
+            //string[] arrSearchCommonCodes = { "RATE", "SCORE", "EXCEPT_CD", "USE_YN", "DIAG_TYPE" };
+            //List<TcommonCode> _ddlCommonCodes = systemCodeRepository.GetCommonCodeByArray(arrSearchCommonCodes, m_UseYn);
+
+            //var vulnEditModel = new VulnEditModel
+            //{
+            //    tCommonCodes = _ddlCommonCodes,
+            //    tCompInfos = vulnDbContext.TcompInfo.ToList(),
+            //    tVulnGroup_kind = vulnDbContext.TvulnGroup.Where(x => x.CompSeq == vulnEditInfo.CompSeq && x.GroupType == "K").Distinct().ToList(),
+            //    tVulnGroup_group = vulnDbContext.TvulnGroup.Where(x => x.DiagKind == vulnEditInfo.DiagKind && x.GroupType == "G").ToList(),
+            //    tVulnEditInfo = vulnEditInfo
+            //};
+
+            return View(t_VULN);
+        }
+
+        [HttpGet]
         public IActionResult VulnList()
         {
             var _ddlDiagType = _systemCodeRepository.GetCommonCodeDropDownList("DIAG_TYPE", true);
@@ -54,8 +95,8 @@ namespace WebCore.Areas.VulnManage.Controllers
             param.Add("@manage_id", "");
             param.Add("@rate", 0);
             param.Add("@score", 0);
-            param.Add("@use_yn", "");
-            param.Add("@exception_yn", "");
+            param.Add("@use_yn", 1);
+            param.Add("@exception_yn", 0);
             param.Add("@user_id", "");
             param.Add("@sort_field", "");
             param.Add("@is_desc", 1);
@@ -66,12 +107,12 @@ namespace WebCore.Areas.VulnManage.Controllers
 
             // 테이블 한개 리스트
             //var initVulnModels = new List<T_VULN> { new T_VULN() }; //DapperHelper.GetList<T_VULN>("SP_VULN_LIST_TEST", param).ToList();
-            var initVulnModels = _dapperHelper.GetList<T_VULN>("SP_VULN_LIST_TEST", param).ToList();
+            var initVulnModels = _dapperHelper.GetList<T_VULN>("SP_VULN_LIST_02", param).ToList();
 
             // 메인 + 서브 모델
             //var Table = SqlHelper.MultiPleGetList<Schema_User, Address>(SqlHelper.localDB.ToString(), "sp_GetUser_List", param);
 
-            int resultParam = 0;// param.Get<int>("@allCount");
+            //int resultParam = 0;// param.Get<int>("@allCount");
 
             VulnManagePageModel vulnViewModel = new VulnManagePageModel
             {
